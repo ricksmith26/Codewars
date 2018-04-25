73628a0a46608a9b0146c8bcc26241c3748422a4
ricksmith26 <ricksmith26@hotmail.com>
2018-04-25 16:01:12 +0100

    omnitool 1xtested

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,15 +1,16 @@
 let path = require("path");
 let expect = require("chai").expect;
-let { sentenceToCamelCase } = require("../index.js");
+let { omnitool } = require("../index.js");
 
 describe("omnitool", () => {
   it("it should return an array without any under the value of 2", () => {
     let inputarray = [11, 1, 5, 45, 99, 1, 6, 88];
-    const call = function(val) {
+    const call = val => {
       return val > 2;
     };
     let expected = [11, 5, 45, 99, 6, 88];
 
-    expect(inputarray).to.eql(expected);
+    let actual = omnitool(inputarray, call);
+    expect(actual).to.eql(expected);
   });
 });
